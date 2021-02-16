9c5cc1e295137b44a9c04e50abf2379887af5223
Emma Grob <eagrob@gmail.com>
2021-02-16 19:59:49 +0200

    cleaned up code

--- src/ipApi.js
+++ src/ipApi.js
@@ -4,8 +4,6 @@ const apiKey = "dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e";
 const baseUrl = `https://api.ipdata.co?api-key=${apiKey}`;
 
 export const getIpData = async () => {
-    const result = await axios.get(baseUrl, function(response) {
-        ("#response").html(JSON.stringify(response, null, 4));
-}, "jsonp");
+    const result = await axios.get(baseUrl, function(response){}, "jsonp");
     return result.data;
 }
