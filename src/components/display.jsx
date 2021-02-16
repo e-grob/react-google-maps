ce812507fc6d4da29cabf0dafdafeba18bec690a
Emma Grob <eagrob@gmail.com>
2021-02-16 12:27:45 +0200

    display user location on map

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getIpData } from "../ipApi";
 
+const googleApiKey = "AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk";
+
 export const Display = () => {
-  const handleButtonClick = async () => {
-    const ipData = await getIpData();
-    console.log(ipData);
-  };
+  const [ipData, setIpData] = useState([]);
+
+  useEffect(async () => {
+    const ipDataResponse = await getIpData();
+    setIpData(ipDataResponse);
+  }, []);
+
   return (
     <div className="display">
-      <h1>Google Maps</h1>
-      <button onClick={() => handleButtonClick()}>Find Location</button>
+      <h1>Your Location</h1>
+      <hr></hr>
+      <h3>
+        {ipData.city}, {ipData.country_name}
+      </h3>
+      <iframe
+        width="600"
+        height="450"
+        frameBorder="0"
+        style={{ border: 0 }}
+        src={`https://www.google.com/maps/embed/v1/place?key=${googleApiKey}
+    &q=${ipData.city},${ipData.country_name}`}
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
